73a399368bb1120644c8ffebc91a4c8a864b307b
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-11 12:42:02 +0200

    preparazione

--- src/main/java/francescaBattistini/Application.java
+++ src/main/java/francescaBattistini/Application.java
@@ -1,8 +1,16 @@
 package francescaBattistini;
 
-public class Application {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Application {
+private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("catalogobibliografico");
+
+public static void main(String[] args) {
+    EntityManager em = emf.createEntityManager();
+
+
+
     }
 }
